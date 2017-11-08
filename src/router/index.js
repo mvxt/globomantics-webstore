af643e8a64b6b25925bb56ddf2954e88d31bd598
Michael Vinh Xuan Thanh <meta.orphic@gmail.com>
2017-11-07 22:57:11 -0700

    Added a few different pages, placeholder navbar

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+
+import Home from '@/components/Home'
+import Cart from '@/components/Cart'
+import Admin from '@/components/Admin'
 
 Vue.use(Router)
 
@@ -8,8 +11,18 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/admin',
+      name: 'Admin',
+      component: Admin
+    },
+    {
+      path: '/cart',
+      name: 'Cart',
+      component: Cart
     }
   ]
 })
