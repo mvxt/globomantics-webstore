a40328d412cde74386827d7c91ea6dd03669a286
Michael Vinh Xuan Thanh <meta.orphic@gmail.com>
2017-11-30 22:25:43 -0700

    Enabled data table list view of all existing products in admin

--- src/pages/admin/Products.vue
+++ src/pages/admin/Products.vue
@@ -1,7 +1,54 @@
 <template>
-  <div>
-    <div class="title">
-      <h1>This is Admin/Products</h1>
-    </div>
-  </div>
+  <v-data-table v-bind:headers="headers"
+                :items="products"
+                hide-actions
+                class="elevation-1">
+    <template slot="items" slot-scope="props">
+      <td width="400px">
+        <img :src="props.item.image" alt="" style="height: 200%; width: 50%;" />
+      </td>
+      <td class="text-xs-right">{{ props.item.name }}</td>
+      <td class="text-xs-right">{{ props.item.description }}</td>
+      <td class="text-xs-right">{{ props.item.upc }}</td>
+      <td class="text-xs-right">{{ props.item.price }}</td>
+      <td class="text-xs-right">
+        <router-link :to="'/edit/' + props.item._id">
+          <v-icon>mode_edit</v-icon>
+        </router-link>
+      </td>
+    </template>
+  </v-data-table>
 </template>
+
+<script>
+  export default {
+    name: 'admin-product-list',
+    data () {
+      return {
+        headers: [
+          {
+            text: 'Image',
+            align: 'left',
+            sortable: false,
+            value: 'image'
+          },
+          { text: 'Name', value: 'name' },
+          { text: 'Description', value: 'description' },
+          { text: 'UPC Code', value: 'upc' },
+          { text: 'Price ($)', value: 'price' },
+          { text: '', value: '' }
+        ]
+      }
+    },
+    created () {
+      if (this.products.length === 0) {
+        this.$store.dispatch('allProducts')
+      }
+    },
+    computed: {
+      products () {
+        return this.$store.getters.allProducts
+      }
+    }
+  }
+</script>
