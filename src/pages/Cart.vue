945144e002c1181890db4490ac9578e7d89dbd7f
Michael Vinh Xuan Thanh <meta.orphic@gmail.com>
2017-12-02 17:31:56 -0700

    Added subtotal and num items above checkout button, put all those contents into a card

--- src/pages/Cart.vue
+++ src/pages/Cart.vue
@@ -7,15 +7,24 @@
           <product-details :product="product"></product-details>
         </v-flex>
       </v-layout>
-      <v-layout row key="checkout">
+      <v-layout row key="checkout" class="mt-2">
         <v-spacer></v-spacer>
-        <v-btn style="height: 50px" @click="">
-          <v-badge overlay left overlap color="green">
-            <v-icon slot="badge">check</v-icon>
-            <v-icon>shopping_cart</v-icon>
-          </v-badge>
-          <span class="mt-1">Checkout</span>
-        </v-btn>
+        <v-card>
+          <v-card-title primary-title>
+            <h3 class="title mb-0 mr-2">Subtotal ({{ products.length }} items): </h3>
+            <span class="title mb-0">{{ subtotal }}</span>
+          </v-card-title>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn style="height: 50px" @click="">
+              <v-badge overlay left overlap color="green">
+                <v-icon slot="badge">check</v-icon>
+                <v-icon>shopping_cart</v-icon>
+              </v-badge>
+              <span class="mt-1">Checkout</span>
+            </v-btn>
+          </v-card-actions>
+        </v-card>
       </v-layout>
     </transition-group>
   </v-container>
@@ -45,13 +54,19 @@
     },
     computed: {
       products () {
-        console.log(this.cart)
         var products = []
         for (var i = 0; i < this.cart.length; i++) {
           var product = Object.assign({}, this.$store.getters.product(this.cart[i]))
           products.push(product)
         }
         return products
+      },
+      subtotal () {
+        var total = 0
+        for (var product in this.products) {
+          total += this.products[product].price
+        }
+        return '$' + total.toFixed(2)
       }
     },
     components: {
